d7924e19ba3ff290e490eca6507af78d646ed861
Ra00f1 <raooffarhang@gmail.com>
2021-03-11 17:33:55 +0300

    room 1 prototype

---
+++ 3D Rougelike (GameJam)/Assets/Scripts/TorchadnDoorSc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchadnDoorSc : MonoBehaviour
+{ 
+    public GameObject[] Walls;
+   void Start()
+    {
+        int rand = Random.Range(0, Walls.Length);
+        Instantiate(Walls[rand], transform.position, transform.rotation);
+    }
+}
